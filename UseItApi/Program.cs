85d59117ad482aaf6b6c77ae0808e97dd69359f9
Ndelforno <nicolas.delforno@intelex.com>
2025-08-25 17:05:35 -0400

    Setup authentication

--- UseItApi/Program.cs
+++ UseItApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins(allowedOrigins) /
+            policy.WithOrigins(allowedOrigins)
                   .AllowAnyMethod()
                   .AllowAnyHeader();
         });
