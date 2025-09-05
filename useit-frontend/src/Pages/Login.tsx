59d2e50dd2b3b7e58ed42413c7babc6b5af1d979
Ndelforno <nicolas.delforno@intelex.com>
2025-09-05 09:40:09 -0400

    fix login

--- useit-frontend/src/Pages/Login.tsx
+++ useit-frontend/src/Pages/Login.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { AuthForm } from "../Components/AuthForm";
-import { loginUser } from "../Api";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Components/AuthContext";
 
 export default function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const navigate = useNavigate();
-  const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const { isLoggedIn, setIsLoggedIn, login } = useAuth();
 
   if (isLoggedIn) {
     navigate("/");
@@ -16,8 +15,7 @@ export default function Login() {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const token = await loginUser(formData.email, formData.password);
-      localStorage.setItem("token", token);
+      login(formData.email, formData.password);
       alert("Login successful!");
       setIsLoggedIn(true);
       navigate("/");
